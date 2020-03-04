d090146e567c78f8a41fa7bb941f3fc9d1ed4410
Nefaris <kamcio190@gmail.com>
2020-03-04 22:04:13 +0100

    basic view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BoardComponent } from './components/board/board.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { ReposComponent } from './components/repos/repos.component';
+import { RepoComponent } from './components/repo/repo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BoardComponent,
+    SearchBarComponent,
+    ReposComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule
